a11b3ffd92afd3649a20303291a4a4e174dbbb44
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-01-12 18:58:19 -0300

    Cliente - Fim aula 01
    cliente model e api

---
+++ src/main/java/com/cursoalgworks/osworks/domain/model/Cliente.java
@@ -0,0 +1,41 @@
+package com.cursoalgworks.osworks.domain.model;
+
+public class Cliente {
+	private Long id;
+	private String nome;
+	private String email;
+	private String telefone;
+	private static long quantidadeCliente = 0;
+	
+	public Cliente() {
+		//incrementa o id do cliente automaticamente
+		this.id = Cliente.quantidadeCliente;
+		Cliente.quantidadeCliente++;
+	}
+	//getters and setters 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+}
