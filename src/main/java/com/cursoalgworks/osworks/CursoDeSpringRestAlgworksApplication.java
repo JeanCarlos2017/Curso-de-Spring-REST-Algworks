32fc693b72e900fee1530fd3c3836ca1be2b5b43
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-01-12 17:59:45 -0300

    start spring.io

---
+++ src/main/java/com/cursoalgworks/osworks/CursoDeSpringRestAlgworksApplication.java
@@ -0,0 +1,13 @@
+package com.cursoalgworks.osworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDeSpringRestAlgworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDeSpringRestAlgworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoalgworks/osworks/CursoDeSpringRestAlgworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoalgworks.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoDeSpringRestAlgworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
