005dc2a2f1254fa473e6c76d5d8800efef54b8aa
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-01-14 20:22:25 -0300

    create table Cliente
    criei a tabela cliente e já fiz algumas inserções no banco de dados

--- src/main/java/com/cursoalgworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/cursoalgworks/osworks/api/controller/ClienteController.java
@@ -3,6 +3,10 @@ package com.cursoalgworks.osworks.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,25 +14,15 @@ import com.cursoalgworks.osworks.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listarClientes() {
-		var cliente = new Cliente();
-		cliente.setNome("janks_2017");
-		cliente.setEmail("janks@email.com");
-		cliente.setTelefone("tel_00");
-		
-		var cliente2 = new Cliente();
-		cliente2.setNome("princesa");
-		cliente2.setEmail("princesa@email.com");
-		cliente2.setTelefone("tel_01");
-		
-		var cliente3 = new Cliente();
-		cliente3.setNome("mamis");
-		cliente3.setEmail("mamusca@email.com");
-		cliente3.setTelefone("tel_02");
-		
-		return Arrays.asList(cliente, cliente2, cliente3);
-		
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
+				
 	}
 
 }
