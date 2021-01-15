a8ef67773f0d11389d1f4a93abf9dbc579e1576c
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-01-15 09:13:45 -0300

    fim CRUD cliente

--- src/main/java/com/cursoalgworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/cursoalgworks/osworks/api/controller/ClienteController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,16 @@ public class ClienteController {
 			cliente.setId(clienteId); //seta o id para o banco ver que o ciente já existe e eu só quero alterá-lo
 			clienteRepository.save(cliente);
 			return ResponseEntity.ok(cliente);
+		}
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> removerCliente(@PathVariable Long clienteId){
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}else {
+			clienteRepository.deleteById(clienteId);
+			return ResponseEntity.noContent().build();
 		}
 	}
 
