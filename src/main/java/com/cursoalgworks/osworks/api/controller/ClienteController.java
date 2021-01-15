16edbbed2fd08b3eb02bb4f0be3bda9b571d062d
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-01-15 13:42:09 -0300

    Validação de dados

--- src/main/java/com/cursoalgworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/cursoalgworks/osworks/api/controller/ClienteController.java
@@ -3,6 +3,8 @@ package com.cursoalgworks.osworks.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,12 +45,12 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionarCliente(@RequestBody Cliente cliente) {
+	public Cliente adicionarCliente(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
-	public ResponseEntity<Cliente> alterarCliente(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> alterarCliente(@Valid @PathVariable Long clienteId, @RequestBody Cliente cliente) {
 		if(!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
 		}else {
