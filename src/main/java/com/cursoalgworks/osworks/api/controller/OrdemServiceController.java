df692e201fc19f5e7c4ef42c6410eb1867f20138
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-01-16 14:09:16 -0300

    Listar e buscar - Ordem Servico

--- src/main/java/com/cursoalgworks/osworks/api/controller/OrdemServiceController.java
+++ src/main/java/com/cursoalgworks/osworks/api/controller/OrdemServiceController.java
@@ -1,9 +1,15 @@
 package com.cursoalgworks.osworks.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursoalgworks.osworks.domain.model.OrdemServico;
+import com.cursoalgworks.osworks.domain.repository.OrdemServicoRepository;
 import com.cursoalgworks.osworks.domain.service.GestaoOrdemServicoService;
 
 @RestController
@@ -20,9 +27,29 @@ public class OrdemServiceController {
 	@Autowired
 	private GestaoOrdemServicoService gestaoOrdemServicoService;
 	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRepository;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
 		return gestaoOrdemServicoService.criar(ordemServico);
 	}
+	
+	@GetMapping
+	public List<OrdemServico> listar(){
+		return ordemServicoRepository.findAll();
+	}
+	
+	@GetMapping("/{ordemServicoId}")
+	public ResponseEntity<OrdemServico> buscaPorId(@PathVariable Long ordemServicoId) {
+		Optional<OrdemServico> ordemServicoOptional =  ordemServicoRepository.findById(ordemServicoId);
+		
+		if (ordemServicoOptional.isPresent()) {
+			return ResponseEntity.ok(ordemServicoOptional.get());
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
 }
